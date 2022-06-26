5194a4b42b0868f0af5f68e284046cd0f622b76f
hamdev05 <99078030+hamdev05@users.noreply.github.com>
2022-06-26 17:34:42 +0100

    updated my calc javascript page

--- calc.js
+++ calc.js
@@ -1,13 +1,9 @@
 const displayScreen = document.getElementById("display");
 
-
 const inputNum = (num) => (displayScreen.textContent += num);
-
 
 const equalTo = () => displayScreen.textContent? (displayScreen.textContent = eval(displayScreen.textContent)) : false;
 
-
 const removeNum = () => (displayScreen.textContent = displayScreen.textContent.substring(0, displayScreen.textContent.length - 1));
-
 
 const clearInput = () => (displayScreen.textContent = "");
